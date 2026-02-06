81c5ce9c969d9418524b9dd9da4103c5547d3048
André Felipe Isensee Leite <felipe_santofortee@hotmail.com>
2026-02-06 14:10:54 -0300

    Praticando tulipas em Python

--- exercicios/ex072_numero_por_extenso.py
+++ exercicios/ex072_numero_por_extenso.py
@@ -1,3 +1,4 @@
+# Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
 extenso = (
     "zero",
     "um",
--- exercicios/ex075_analise_dados.py
+++ exercicios/ex075_analise_dados.py
@@ -1,3 +1,7 @@
+# Crie um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. No final, mostre:
+# A) Quantas vezes apareceu o valor 9.
+# B) Em que posição foi digitado o primeiro valor 3.
+# C) Quais foram os números pares.
 valor = (
     int(input("Digite um valor inteiro: ")),
     int(input("Digite outro valor inteiro: ")),
